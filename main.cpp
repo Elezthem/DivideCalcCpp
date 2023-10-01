263f416219df0b8db8d478b924b16ec37253d80e
ElezthemDev <97851451+Elezthem@users.noreply.github.com>
2023-10-01 19:23:18 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+void divide(float a, float b) {
+	if (b == 0) throw 444;
+	else cout << (a / b);
+}
+
+int main() {
+	setlocale(LC_ALL, "RU");
+
+	try {
+		divide(5.2f, 0.0f);
+	}
+	catch (int error) {
+		if (error == 444) cout << "Я не могу выплнить деление на 0!";
+	}
+
+	return 0;
+}
